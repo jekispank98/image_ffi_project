632d1b250b7f0b0e6a97743beaa8cc913256bf90
Jekis <e.osipov@radioavionica.ru>
2026-01-30 02:12:46 -0500

    Add args parser. Add image dependencies. Write is_file_exist fun

--- image_processor/src/main.rs
+++ image_processor/src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::path::PathBuf;
+use clap::Parser;
+use image::ImageReader;
+use crate::args::Args;
+
+pub mod args;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let file_path = args.input;
+    if check_is_image_exist(&file_path) {
+        let img = ImageReader::open(&file_path).expect("Couldn't open the image").decode();
+    }
+}
+
+fn check_is_image_exist(path: &String) -> bool {
+    let path_buf = PathBuf::from(path);
+    path_buf.exists()
 }
